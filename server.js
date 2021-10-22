fb4cb487b317286b3d1104e432ccbb38071dcc11
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-10-22 15:30:38 -0700

    add initial files + databases

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mysql = require('mysql2');
+const api = require('./routs/index')
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: 'password',
+    database: 'employees_db'
+  },
+  console.log(`Connected to the employees_db database.`)
+);
+
+
+app.use('/api', api)
+
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
