1934090c30b6e27b655cb9745b59bf7cf47d7e0d
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-10-23 14:08:35 -0700

    testing databse foreign key and references

--- db/schema.sql
+++ db/schema.sql
@@ -10,20 +10,20 @@ CREATE TABLE department (
 
 CREATE TABLE role (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(30),
-    salary Decimal,
-    review TEXT,
+    title VARCHAR(30) NOT NULL,
+    salary Decimal NOT NULL,
     department_id INT,
-    -- FOREIGN KEY ()
-    -- REFERENCES (id)
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    -- FOREIGN KEY ()
-    -- REFERENCES (id)
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
 );
